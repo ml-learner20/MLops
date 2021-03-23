175c81a016fed6f89e09bf1e52bf5f6530cc5b67
ML Learner <ourprincess3@gmail.com>
2021-03-23 13:21:15 -0400

    test updated

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -75,7 +75,15 @@ def api_response(dict_request):
             response = predict(data)
             response = {"response": response}
             return response
+
+    except NotInRange as e:
+        response = {"the_exected_range": get_schema(), "response": str(e)}
+        return response
+
+    except NotInCols as e:
+        response = {"the_exected_cols": get_schema().keys(), "response": str(e)}
+        return response
+
     except Exception as e:
-        print(e)
-        response = {"the_expected_range": get_schema(),"response": str(e)}
+        response = {"response": str(e)}
         return response
